0963cedca026a69f5bb588a6f24f31f3e25f58bf
Max <marcgiu@ttc.cat>
2017-03-23 10:41:56 +0100

    max

---
+++ SDL5_Handout/ModuleSceneKen.h
@@ -0,0 +1,34 @@
+#ifndef __MODULESCENEKEN_H__
+#define __MODULESCENEKEN_H__
+
+#include "Module.h"
+#include "Animation.h"
+#include "Globals.h"
+#include "ModuleInput.h"
+
+struct SDL_Texture;
+
+class ModuleSceneKen : public Module
+{
+public:
+	ModuleSceneKen();
+	~ModuleSceneKen();
+
+	bool Start();
+	update_status Update();
+	bool CleanUp();
+
+public:
+	
+	SDL_Texture* graphics = nullptr;
+	SDL_Rect ground;
+	SDL_Rect foreground;
+	SDL_Rect background;
+	Animation flag;
+	Animation girl;
+	bool fading;
+	float foreground_pos;
+	bool forward;
+};
+
+#endif // __MODULESCENEKEN_H__
